470695b3708ecdf12021f00dd7ee508a7398e932
ajay kumar <ajaysahai007@gmail.com>
2023-03-22 14:54:04 +0530

    updated

--- experience/script.js
+++ experience/script.js
@@ -45,15 +45,15 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 // Start of Tawk.to Live Chat
 //<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/63f7699d31ebfa0fe7eee96a/1gpv794b3';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/63f7699d31ebfa0fe7eee96a/1gpv794b3';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 //</script>
 // End of Tawk.to Live Chat
 
